5137f2ed4199905229c541030718c2c504862dba
jeffarjeffar <jeffarjeffar@gmail.com>
2023-03-24 09:50:55 -0700

    complete stocks

--- Stock.java
+++ Stock.java
@@ -16,7 +16,148 @@ public class Stock {
     private int volume;
     private PriorityQueue<TradeOrder> buyOrders, sellOrders;
 
-    // TODO complete class
+    /**
+     * Constructs a new stock with a given symbol, company name,
+     * and starting price. Sets low price, high price,
+     * and last price to the same opening price.
+     * Sets "day" volume to zero.
+     * Initializes a priority queue for sell orders
+     * to an empty <code>PriorityQueue</code> with a <code>PriceComparator</code>
+     * configured for comparing orders in ascending order;
+     * initializes a priority queue for buy orders
+     * to an empty <code>PriorityQueue</code> with a <code>PriceComparator</code>
+     * configured for comparing orders in descending order.
+     */
+    public Stock(String symbol, String name, double price) {
+        stockSymbol = symbol;
+        companyName = name;
+        loPrice = price;
+        hiPrice = price;
+        lastPrice = price;
+        volume = 0;
+        buyOrders = new PriorityQueue<TradeOrder>(11, new PriceComparator(false));
+        sellOrders = new PriorityQueue<TradeOrder>(11, new PriceComparator(true));
+    }
+
+    /**
+     * Returns a quote string for this stock. The quote includes:
+     * the company name for this stock; the stock symbol; last sale price;
+     * the lowest and highest day prices; the lowest price in
+     * a sell order (or "market") and the number of shares in it
+     * (or "none" if there are no sell orders); the highest
+     * price in a buy order (or "market") and the number of shares in it
+     * (or "none" if there are no buy orders).
+     * For example:<pre>
+     * Giggle.com (GGGL)
+     * Price: 10.00  hi: 10.00  lo: 10.00  vol: 0
+     * Ask: 12.75 size: 300  Bid: 12.00 size: 500</pre>Or:<pre>
+     * Giggle.com (GGGL)
+     * Price: 12.00  hi: 14.50  lo: 9.00  vol: 500
+     * Ask: none  Bid: 12.50 size: 200</pre>
+     */
+    public String getQuote() {
+        return String.format("%s (%s)\nPrice: %.2f  hi: %.2f  lo: %.2f  vol: %d\nAsk: %s  Bid: %s", companyName,
+                             stockSymbol, lastPrice, hiPrice, loPrice, volume, formatOrder(sellOrders.peek()),
+                             formatOrder(buyOrders.peek()));
+    }
+
+    private String formatOrder(TradeOrder order) {
+        if (order == null) {
+            return "none";
+        }
+        return String.format("%.2f size: %d", order.getPrice(), order.getShares());
+    }
+
+    /**
+     * Places a trading order for this stock.
+     * Adds the order to the appropriate priority queue depending
+     * on whether this is a buy or sell order.
+     * Notifies the trader
+     * who placed the order that the order has been placed, by sending a
+     * message to that trader. For example:
+     * <pre>
+     * New order:  Buy GGGL (Giggle.com)
+     * 200 shares at $38.00</pre>
+     * Or, for market orders:
+     * <pre>
+     * New order:  Sell GGGL (Giggle.com)
+     * 150 shares at market</pre>
+     * Executes pending orders by calling
+     * <code>executeOrders</code>.
+     */
+    public void placeOrder(TradeOrder order) {
+        if (order.isBuy()) {
+            buyOrders.add(order);
+        } else {
+            assert (order.isSell());
+            sellOrders.add(order);
+        }
+        executeOrders();
+    }
+
+    /**
+     * Executes as many pending orders as possible. <br>
+     * <ol type="1">
+     *   <li> Examines the top sell order and the top buy order in
+     *      the respective priority queues.</li>
+     * <ol type="i">
+     *      <li>If both are limit orders and the buy order price is greater or equal
+     *      to the sell order price, executes the order (or a part of it)
+     *      at the sell order price.</li>
+     *      <li>If one order is limit and the other is market, executes the
+     *      order (or a part of it) at the limit order price</li>
+     *      <li>If both orders are market, executes the order (or a part of it)
+     *      at the last sale price.</li>
+     * </ol>
+     *   <li> Figures out how many shares can be traded, which
+     *      is the smallest of the numbers of shares in the two orders.</li>
+     *   <li> Subtracts the traded number of shares from each order;
+     *      Removes each of the orders with 0 remaining shares from the respective queue.</li>
+     *   <li> Updates the day's low price, high price, and volume.</li>
+     *   <li> Sends a message to each of the two traders involved in the transaction.
+     *      For example:
+     *      <pre>
+     *      You bought: 150 GGGL at 38.00 amt 5700.00</pre>
+     *
+     *   <b>Note:</b> The dollar amounts should be formatted to two decimal places (eg. 12.40, not 12.4)<br>
+     *   <br>
+     *   <li> Repeats steps 1-5 for as long as possible, that is as long as
+     *      there is any movement in the buy / sell order queues.
+     *      (The process gets stuck when the top buy order and sell order
+     *      are both limit orders and the ask price is higher than the bid
+     *      price.)</li>
+     * </ol>
+     */
+    public void executeOrders() {
+        TradeOrder buyOrder = buyOrders.peek();
+        TradeOrder sellOrder = sellOrders.peek();
+        int sharesTraded = Math.min(buyOrder.getShares(), sellOrder.getShares());
+        double price = 0;
+        if (buyOrder.isLimit() && sellOrder.isLimit() && buyOrder.getPrice() >= sellOrder.getPrice()) {
+            price = sellOrder.getPrice();
+        } else if (buyOrder.isLimit() && sellOrder.isMarket()) {
+            price = buyOrder.getPrice();
+        } else if (buyOrder.isMarket() && sellOrder.isLimit()) {
+            price = sellOrder.getPrice();
+        } else {
+            price = lastPrice;
+        }
+        buyOrder.subtractShares(sharesTraded);
+        sellOrder.subtractShares(sharesTraded);
+        if (buyOrder.getShares() == 0) {
+            buyOrders.remove();
+        }
+        if (sellOrder.getShares() == 0) {
+            sellOrders.remove();
+        }
+        hiPrice = Math.max(hiPrice, price);
+        loPrice = Math.min(loPrice, price);
+        volume += sharesTraded;
+        buyOrder.getTrader().receiveMessage(String.format("You bought: %d %s at %.2f amt %.2f", sharesTraded,
+                                                          stockSymbol, price, sharesTraded * price));
+        sellOrder.getTrader().receiveMessage(
+            String.format("You sold: %d %s at %.2f amt %.2f", sharesTraded, stockSymbol, price, sharesTraded * price));
+    }
 
     //
     // The following are for test purposes only
--- StockExchange.java
+++ StockExchange.java
@@ -12,28 +12,29 @@ import java.util.*;
 public class StockExchange {
     private Map<String, Stock> listedStocks;
 
-	/**
-	 * Constructs a new stock exchange object.
-	 */
+    /**
+     * Constructs a new stock exchange object.
+     */
     public StockExchange() {
         listedStocks = new HashMap<String, Stock>();
     }
 
-	/**
-	 * Adds a new stock with given parameters to the listed stocks.
-	 * @param symbol stock symbol
-	 * @param name full company name
-	 * @param price opening stock price
-	 */
+    /**
+     * Adds a new stock with given parameters to the listed stocks.
+     * @param symbol stock symbol
+     * @param name full company name
+     * @param price opening stock price
+     */
     public void listStock(String symbol, String name, double price) {
         listedStocks.put(symbol, new Stock(symbol, name, price));
     }
 
-	/**
-	 * Returns a quote for a given stock. If the symbol (ex. XYZ) is not found in the exchange's list of stocks, the string that is returned should be "XYZ not found".
-	 * @param symbol stock symbol
-	 * @return a text message that contains the quote.
-	 */
+    /**
+     * Returns a quote for a given stock. If the symbol (ex. XYZ) is not found in the exchange's list of stocks, the
+     * string that is returned should be "XYZ not found".
+     * @param symbol stock symbol
+     * @return a text message that contains the quote.
+     */
     public String getQuote(String symbol) {
         Stock stock = listedStocks.get(symbol);
         if (stock == null) {
@@ -42,10 +43,12 @@ public class StockExchange {
         return stock.getQuote();
     }
 
-	/**
-	 * Places a trade order by calling <code>stock.placeOrder</code> for the stock specified by the stock symbol in the trade order. If the stock (ex. XYZ) is not found in the exchange's list of stocks, then the exchange sends a message to the trader with the message "XYZ not found".
-	 * @param order a trading order to be placed with this stock exchange.
-	 */
+    /**
+     * Places a trade order by calling <code>stock.placeOrder</code> for the stock specified by the stock symbol in the
+     * trade order. If the stock (ex. XYZ) is not found in the exchange's list of stocks, then the exchange sends a
+     * message to the trader with the message "XYZ not found".
+     * @param order a trading order to be placed with this stock exchange.
+     */
     public void placeOrder(TradeOrder order) {
         Stock stock = listedStocks.get(order.getSymbol());
         stock.placeOrder(order);
@@ -54,6 +57,10 @@ public class StockExchange {
     //
     // The following are for test purposes only
     //
+    /**
+     * Get the listed stocks
+     * @return listedStocks
+     */
     protected Map<String, Stock> getListedStocks() {
         return listedStocks;
     }
